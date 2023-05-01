5813004f7d3465f4789a4b46bf3b0ce6ef181958
MovingLlama <stefan@seyerl.eu>
2023-05-01 22:10:18 +0200

    First Setup

--- src/setup.h
+++ src/setup.h
@@ -1,7 +1,7 @@
 //Setup your credentials and mqtt info here:
 //only change the value between the " " leave the rest of the line untouched.
-#define WIFI_SSID "SSID"//**Your SSID here**
-#define WIFI_PWD "password"//**Your password here** leave empty if open (bad!)
+#define WIFI_SSID "TheLANdownunder"//**Your SSID here**
+#define WIFI_PWD "S3bast1an"//**Your password here** leave empty if open (bad!)
 
 //Uncomment this to set a static IP instead of DHCP for the ESP (Separate by commas instead of dots)
 //#define WIFI_IP 192, 168, 0, 5
@@ -10,12 +10,12 @@
 //#define WIFI_PRIMARY_DNS 8, 8, 8, 8     //optional
 //#define WIFI_SECONDARY_DNS 8, 8, 4, 4   //optional
 
-#define MQTT_SERVER "192.168.1.4"//**IP address here of your MQTT server**
-#define MQTT_USERNAME ""//leave empty if not set (bad!)
-#define MQTT_PASSWORD ""//leave empty if not set (bad!)
+#define MQTT_SERVER "mqtt.local.seyerl.eu"//**IP address here of your MQTT server**
+#define MQTT_USERNAME "espaltherma"//leave empty if not set (bad!)
+#define MQTT_PASSWORD "espaltherma"//leave empty if not set (bad!)
 #define MQTT_PORT 1883
 
-#define FREQUENCY 30000 //query values every 30 sec
+#define FREQUENCY 10000 //query values every 30 sec
 
 #if defined(ARDUINO_M5Stick_C) || defined(ARDUINO_M5Stick_C_Plus)
 //Values used when **M5StickC** or **M5STickCPlus** environment is selected:
@@ -31,8 +31,8 @@
 
 //Smart grid control - Optional:
 //Uncomment and set to enable SG mqtt functions
-//#define PIN_SG1 32// Pin connected to dry contact SG 1 relay (normally open)
-//#define PIN_SG2 33// Pin connected to dry contact SG 2 relay (normally open)
+#define PIN_SG1 32// Pin connected to dry contact SG 1 relay (normally open)
+#define PIN_SG2 33// Pin connected to dry contact SG 2 relay (normally open)
 // Define if your SG relay board is Low or High triggered (signal pins)
 // Only uncomment one of them
 #define SG_RELAY_HIGH_TRIGGER
@@ -47,14 +47,14 @@
 #define SG_RELAY_INACTIVE_STATE LOW
 #endif
 
-#define MAX_MSG_SIZE 7120//max size of the json message sent in mqtt 
+#define MAX_MSG_SIZE 15120//max size of the json message sent in mqtt 
 
 //Uncomment this line if the JSON message should be in a Json Table format []. Use only for IOBroker Vis. 
 //#define JSONTABLE
 
 //Uncomment this if you want to activate the One Value <-> One Topic mode. Each value will be sent to a specific topic below 
-// #define ONEVAL_ONETOPIC
-// #define MQTT_OneTopic "espaltherma/OneATTR/" //Keep the ending "/" !!
+//#define ONEVAL_ONETOPIC
+//#define MQTT_OneTopic "espaltherma/OneATTR/" //Keep the ending "/" !!
 
 
 //Uncomment *ONE* of the following according to your installation.
@@ -90,11 +90,12 @@
 //#include "def/PROTOCOL_S_ROTEX.h"
 //#include "def/PROTOCOL_S.h"
 //#include "def/DEFAULT.h"
+#include "def/German/ALTHERMA(MONOBLOC_CA_05-07KW).h"
 
-#ifndef LABELDEF
-#warning "NO DEFINITION SELECTED: Please select your heat pump definition in /src/setup.h -- Using default."
-#include "def/DEFAULT.h"
-#endif
+//#ifndef LABELDEF
+//#warning "NO DEFINITION SELECTED: Please select your heat pump definition in /src/setup.h -- Using default."
+//#include "def/DEFAULT.h"
+//#endif
 
 // Default to "Protocol I"
 #ifndef PROTOCOL
